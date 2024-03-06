7d29b788d4b4c9837c644ee1a72d86e9e181b949
BohdanVives <bohdan.shevchenko@student.vives.be>
2024-03-06 09:43:42 +0100

    f

--- Personlijke oefening/Java Script tutorial/scripts/code.js
+++ Personlijke oefening/Java Script tutorial/scripts/code.js
@@ -1,8 +1,31 @@
 const setup = () => {
-    document.write("Hello world! ")
-    console.log("Hello world")
-    console.info("You are very goed")
-    console.error("NOOOOOO")
-    console.warn("Attension")
+    const redRange = document.getElementById('redRange');
+    const greenRange = document.getElementById('greenRange');
+    const blueRange = document.getElementById('blueRange');
+    const redValue = document.getElementById('redValue');
+    const greenValue = document.getElementById('greenValue');
+    const blueValue = document.getElementById('blueValue');
+    const colorDemo = document.getElementById('colorDemo');
+    const orangeRange = document.getElementById("orangeRage");
+    const orangeValue = document.getElementById("orangeValue");
+    function updateColor() {
+        const red = redRange.value;
+        const green = greenRange.value;
+        const blue = blueRange.value;
+        const orange = orangeRange.value;
+        colorDemo.style.backgroundColor = `rgb(${red}, ${green}, ${blue}, ${orange})`;
+
+        redValue.textContent = red;
+        greenValue.textContent = green;
+        blueValue.textContent = blue;
+        orangeValue.textContent = orange;
+    }
+
+    redRange.addEventListener('input', updateColor);
+    greenRange.addEventListener('input', updateColor);
+    blueRange.addEventListener('input', updateColor);
+    orangeRange.addEventListener("input", updateColor)
+
+    updateColor();
 }
 window.addEventListener("load", setup);
--- Tweede semester/Labo2/Opdracht 5/scripts/code.js
+++ Tweede semester/Labo2/Opdracht 5/scripts/code.js
@@ -5,5 +5,10 @@ const setup = () => {
         let pElement = document.getElementById("txtOutput");
         pElement.innerHTML = "Welkom!";
     });
+    let wijzigButtonTerug = document.getElementById("wijzig terug");
+    wijzigButtonTerug.addEventListener("click", () =>{
+        let paElement = document.getElementById("txtOutput");
+        paElement.innerHTML = "Hello world";
+    });
 }
 window.addEventListener("load", setup);
