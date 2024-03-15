88ede813143c48aeec983e348724dadd555132ba
BohdanVives <bohdan.shevchenko@student.vives.be>
2024-03-15 19:35:02 +0100

    s

--- Tweede semester/Labo 5/Opdracht 4/scripts/code.js
+++ Tweede semester/Labo 5/Opdracht 4/scripts/code.js
@@ -1,6 +1,26 @@
 const setup = () => {
-    Gemeenten = [];
+    const gemeenteKiezen = document.getElementById("gemeente");
+    const Gemeenten = [];
 
+    const schrijfGemeente = () => {
+        const gemeente = prompt("Voeg een nieuwe gemeente toe");
+        if (gemeente && gemeente.toLowerCase() !== "stop") {
+            Gemeenten.push(gemeente);
+            Gemeenten.sort((a, b) => a.localeCompare(b));
+            refreshGemeente();
+            schrijfGemeente();
+        } else if (gemeente === null) {
+            alert("Error: " + Gemeenten.join("\n"));
+        } else {
+            alert("Gemeenten:\n" + Gemeenten.join("\n"));
+        }
+    };
 
-}
+    const refreshGemeente = () => {
+        gemeenteKiezen.innerHTML = Gemeenten.map(gemeente => `<option>${gemeente}</option>`).join('');
+    };
+
+    schrijfGemeente();
+};
+
 window.addEventListener("load", setup);
