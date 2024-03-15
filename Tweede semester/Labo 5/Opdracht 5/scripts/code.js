8f3c96c366e6be121f56feb9a50a10774f362537
BohdanVives <bohdan.shevchenko@student.vives.be>
2024-03-15 21:20:27 +0100

    n

--- Tweede semester/Labo 5/Opdracht 5/scripts/code.js
+++ Tweede semester/Labo 5/Opdracht 5/scripts/code.js
@@ -1,4 +1,24 @@
 const setup = () => {
+    const resultaat = document.getElementById("resultaat")
+    resultaat.addEventListener("click", () => {
+    const roker = document.getElementById("roker").checked;
+    const moedertaal = document.querySelector('input[name="taal"]:checked').value;
+    const favoritebuurland = document.getElementById("buurland").value;
+    const food = document.getElementById("food").selectedOptions;
+    const bestelling = [];
+    for (let i = 0; i < food.length; i++) {
+        bestelling.push(food[i].value);
+    }
+    if(roker){
+        console.log("Is roker:", roker);
+    } else {
+        console.log("Is geen roker")
+    }
+    console.log("Moedertaal:", moedertaal);
+    console.log("Favoriete buurland:", favoritebuurland);
+    console.log("Bestelling:", bestelling);
 
+});
 }
+
 window.addEventListener("load", setup);
