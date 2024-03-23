ab800ee599c9f54f90d59b940685e0ccf9101fc2
BohdanVives <bohdan.shevchenko@student.vives.be>
2024-03-23 12:15:34 +0100

    b

--- Tweede semester/Labo 6/Nodes/Opdracht 2/scripts/code.js
+++ Tweede semester/Labo 6/Nodes/Opdracht 2/scripts/code.js
@@ -6,5 +6,15 @@ const setup = () => {
     let foto = document.createElement("img")
     foto.setAttribute("src", "foto/photo_2024-03-22_22-45-55.jpg")
     document.body.append(foto)
+
+
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+            let videoElement = document.getElementById('camera-preview');
+            videoElement.srcObject = stream;
+        })
+        .catch(error => {
+            console.error('Error accessing camera:', error);
+        });
 }
 window.addEventListener("load", setup);
