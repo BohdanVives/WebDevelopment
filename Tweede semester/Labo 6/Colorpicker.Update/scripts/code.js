9e6ea750ba94e8a4d3b410b6ff08ca31fe35e234
BohdanVives <bohdan.shevchenko@student.vives.be>
2024-03-23 12:02:27 +0100

    s

---
+++ Tweede semester/Labo 6/Colorpicker.Update/scripts/code.js
@@ -0,0 +1,49 @@
+const initialize = () => {
+	let sliders = document.getElementsByClassName("slider");
+	for (let i = 0; i < sliders.length; i++) {
+		sliders[i].addEventListener("change", update);
+		sliders[i].addEventListener("input", update);
+	}
+	let button = document.getElementById("button");
+	button.addEventListener("click", voegSwatchToe);
+};
+
+const update = () => {
+	let red = document.getElementById("sldRed").value;
+	let green = document.getElementById("sldGreen").value;
+	let blue = document.getElementById("sldBlue").value;
+	document.getElementById("lblRed").innerHTML = red;
+	document.getElementById("lblGreen").innerHTML = green;
+	document.getElementById("lblBlue").innerHTML = blue;
+	let swatch = document.getElementById("swatch");
+	swatch.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+};
+
+const voegSwatchToe = () => {
+	let red = document.getElementById("sldRed").value;
+	let green = document.getElementById("sldGreen").value;
+	let blue = document.getElementById("sldBlue").value;
+	let swatch = document.getElementById("swatch");
+
+	let nieiweSwatch = document.createElement("div");
+	nieiweSwatch.classList.add("swatch");
+	nieiweSwatch.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+
+	let verwijderen = document.createElement("button");
+	verwijderen.textContent = "x";
+	nieiweSwatch.append(verwijderen);
+
+	const weg = () => {
+		swatch.removeChild(nieiweSwatch);
+	};
+
+	verwijderen.addEventListener("click", weg);
+
+	let newColor = document.getElementById("newcolor")
+
+
+	nieiweSwatch.append(verwijderen);
+	newColor.append(nieiweSwatch);
+};
+
+window.addEventListener("load", initialize);
--- Tweede semester/Labo 6/Nodes/Opdracht 3/scripts/code.js
+++ Tweede semester/Labo 6/Nodes/Opdracht 3/scripts/code.js
@@ -1,13 +1,14 @@
+
 const setup = () => {
-
-    let paragraf = document.createElement("p")
-    let text = document.createTextNode("dat is nieuwe p element")
-    paragraf.append(text)
-
-    let div = document.getElementById("myDIV")
-    document.getElementById("button").document.addEventListener("click", begin)
     const begin = () => {
+        let paragraf = document.createElement("p")
+        let text = document.createTextNode("dat is nieuwe p element")
+        paragraf.append(text)
+
+        let div = document.getElementById("myDIV")
         div.append(paragraf)
     }
+    let button = document.getElementById("button");
+    button.addEventListener("click", begin);
 }
 window.addEventListener("load", setup);
